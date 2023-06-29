18a1738eaaf7cd9ced1cf522301c7bdc5723f195
Giuseppe <133390951+CRICRICode@users.noreply.github.com>
2023-06-29 17:07:47 +0200

    Aggiunte le notazioni per la sicurezza

--- src/main/java/com/sisinfo/Controller/EmployeeController.java
+++ src/main/java/com/sisinfo/Controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.sisinfo.Controller;
 import com.sisinfo.Entity.Employee;
 import com.sisinfo.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -11,22 +12,25 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-
+    @PreAuthorize("hasRole('client admin')")
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
+    @PreAuthorize ("hasRole('client admin')")
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
         return employeeService.getEmployeeDTO(id);
     }
 
+    @PreAuthorize ("hasRole('client admin')")
     @GetMapping("/name/{name}")
     public Employee getEmployeeByName(@PathVariable String name) {
         return employeeService.getEmployeeByName(name);
     }
 
+    @PreAuthorize ("hasRole('client admin')")
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         // Check if an employee with the same email, name, or telephone already exists
@@ -38,11 +42,13 @@ public class EmployeeController {
         return employeeService.createEmployee(employee);
     }
 
+    @PreAuthorize ("hasRole('client admin')")
     @PutMapping("/update/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
         return employeeService.updateEmployee(id, employee);
     }
 
+    @PreAuthorize ("hasRole('client admin')")
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
